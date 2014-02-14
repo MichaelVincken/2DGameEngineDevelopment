4112b99096b79c222cbae92c92195be4c02c7d8a
MichaelVincken <michaelvinckenbe@gmail.com>
2014-02-14 02:58:55 +0100

    Multiplayer Support: The Login Packet.

---
+++ Java2DGame/src/be/vinckenmichael/game/entities/PlayerMP.java
@@ -0,0 +1,32 @@
+package be.vinckenmichael.game.entities;
+
+import java.net.InetAddress;
+
+import be.vinckenmichael.game.InputHandler;
+import be.vinckenmichael.game.level.Level;
+
+public class PlayerMP extends Player {
+
+	public InetAddress ipAddress;
+	public int port;
+
+	public PlayerMP(Level level, int x, int y, InputHandler input,
+			String username, InetAddress ipAddress, int port) {
+		super(level, x, y, input, username);
+		this.ipAddress = ipAddress;
+		this.port = port;
+	}
+
+	public PlayerMP(Level level, int x, int y, String username,
+			InetAddress ipAddress, int port) {
+		super(level, x, y, null, username);
+		this.ipAddress = ipAddress;
+		this.port = port;
+	}
+
+	@Override
+	public void tick() {
+		super.tick();
+	}
+
+}
